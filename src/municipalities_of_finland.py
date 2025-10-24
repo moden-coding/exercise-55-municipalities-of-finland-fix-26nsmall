c444705ea8277c9c1e10717fbe6170c4aff95445
26nsmall <26nsmall@gannacademy.org>
2025-10-24 09:38:30 -0400

    github

--- src/municipalities_of_finland.py
+++ src/municipalities_of_finland.py
@@ -3,7 +3,8 @@
 import pandas as pd
 
 def municipalities_of_finland():
-    pass
+    dataframe = pd.read_csv("src\municipal.tsv",sep="\t", index_col=0)
+    return dataframe["Akaa": "Äänekoski"]
     
 def main():
     print(municipalities_of_finland())
